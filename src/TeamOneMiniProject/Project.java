ef675a7abf7dd4a595723de491490f3b1a211994
Idrees <ahmadid@student.chalmers.se>
2019-12-17 18:44:09 +0100

    removed the Milestone

--- src/TeamOneMiniProject/Project.java
+++ src/TeamOneMiniProject/Project.java
@@ -8,18 +8,6 @@ public class Project {
     private String projectName;
     private int startWeek;
     private int endWeek;
-    private int[] milestone;
-
-    public Project(String projectName, int startWeek, int endWeek, int[] milestone) {
-        this.projectName = projectName;
-        this.startWeek = startWeek;
-        this.endWeek = endWeek;
-        this.milestone = milestone;
-    }
-
-    public Project(){
-
-    }
 
     public String getProjectName() {
         return projectName;
@@ -33,23 +21,6 @@ public class Project {
         return endWeek;
     }
 
-    public int[] getMilestone() {
-        return milestone;
-    }
-
-    public int getMilestoneValue(int i){
-        return milestone[i];
-    }
-
-    @Override
-    public String toString() {
-        return "Project{" +
-                "projectName='" + projectName + '\'' +
-                ", startWeek=" + startWeek +
-                ", endWeek=" + endWeek +
-                ", milestone=" + Arrays.toString(milestone) +
-                '}';
-    }
 }
 
 
