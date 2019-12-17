8168e89f494b7423e16c380009a50c15680859ca
Idrees <ahmadid@student.chalmers.se>
2019-12-17 18:54:57 +0100

    adding testing code

--- src/test/TestIdrees.java
+++ src/test/TestIdrees.java
@@ -21,22 +21,19 @@ public class TestIdrees {
 
     public static void main(String[] args) {
         Project sunShine = convertJsonToObject("C:\\Users\\heaven\\IdeaProjects\\project-group-1\\src\\JsonFiles\\ProjectSunShine.json", Project.class);
+        Workpackage[] workPackages = convertJsonToObject("C:\\Users\\heaven\\IdeaProjects\\project-group-1\\src\\JsonFiles\\WorkPackages.json", Workpackage[].class);
         Member[] members = convertJsonToObject("C:\\Users\\heaven\\IdeaProjects\\project-group-1\\src\\JsonFiles\\Members.json", Member[].class);
-        System.out.println("------------------------------------------------");
         Risk[] risks = convertJsonToObject("C:\\Users\\heaven\\IdeaProjects\\project-group-1\\src\\JsonFiles\\Risks.json", Risk[].class);
+        System.out.println("------------------------------------------------");
         RiskMatrices(risks);
         System.out.println("------------------------------------------------");
-        Workpackage[] workPackages = convertJsonToObject("C:\\Users\\heaven\\IdeaProjects\\project-group-1\\src\\JsonFiles\\WorkPackages.json", Workpackage[].class);
         SV( workPackages, 2);
         System.out.println("------------------------------------------------");
-        System.out.println("check");
         AllSV(workPackages, members);
         System.out.println("------------------------------------------------");
-
         CV( workPackages, 4);
         System.out.println("------------------------------------------------");
         AllCV(workPackages,members);
-
         System.out.println("------------------------------------------------");
 
 //        BarChartRisk chart = new BarChartRisk("Risk Matrix",
@@ -47,11 +44,11 @@ public class TestIdrees {
 
 
 
-        BarCharRisk1 chart01 = new BarCharRisk1("Risk Matrix",
-                "Project Sun Shine");
-        chart01.pack( );
-        RefineryUtilities.centerFrameOnScreen( chart01 );
-        chart01.setVisible( true );
+//        BarCharRisk1 chart01 = new BarCharRisk1("Risk Matrix",
+//                sunShine.getProjectName());
+//        chart01.pack( );
+//        RefineryUtilities.centerFrameOnScreen( chart01 );
+//        chart01.setVisible( true );
 
 
     }
