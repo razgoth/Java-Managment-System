c0443e0481d6fd34111838322ff7249f8b9f27e0
frejat <frejat@student.chalmers.se>
2019-12-18 17:57:02 +0100

    Update Calculations.java

--- src/TeamOneMiniProject/Calculations.java
+++ src/TeamOneMiniProject/Calculations.java
@@ -122,22 +122,7 @@ public class Calculations {
 		return wp;
 	}
 	
-=======
 
-    public static double EV(Member[] members, Workpackage[] workpackages, int weeks) {
-        double EV = 0;
-        int week = weeks - 1;
-        if (weeks > 0 && weeks < workpackages.length&& (weeks%2==0)) {
-            double estimated = workpackages[week].getEstimatedHours() + workpackages[week + 1].getEstimatedHours();
-            double worked = workpackages[week].getWorkedHours() + workpackages[week + 1].getWorkedHours();
-            int workers = workpackages[week].getId().length + workpackages[week + 1].getId().length;
-            int salary = members[1].getCostPerHour();
-            EV = (estimated / worked) * workers * salary;
-        } else {
-            System.out.println("The week you entered is invalid ");
-        }
-        return EV;
-    }
     
 >>>>>>> src/TeamOneMiniProject/Calculations.java
 }
