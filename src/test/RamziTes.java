a07c6e2d5e567180bb9af850d5722c821e0f7a86
sramzi <sramzi@student.chalmers.se>
2019-12-18 17:06:22 +0100

    changed the if statement

--- src/test/RamziTes.java
+++ src/test/RamziTes.java
@@ -7,17 +7,17 @@ import static TeamOneMiniProject.JsonFileHandling.convertJsonToObject;
 
 public class RamziTes {
 
-    public static double sprintEV(Member[] members, Workpackage[] workpackages, int weeks) {
+    public static double EV(Member[] members, Workpackage[] workpackages, int weeks) {
         double EV = 0;
         int week = weeks - 1;
-        if (weeks > 0 && weeks < workpackages.length) {
+        if (weeks > 0 && weeks < workpackages.length&& (weeks%2==0)) {
             double estimated = workpackages[week].getEstimatedHours() + workpackages[week + 1].getEstimatedHours();
             double worked = workpackages[week].getWorkedHours() + workpackages[week + 1].getWorkedHours();
             int workers = workpackages[week].getId().length + workpackages[week + 1].getId().length;
             int salary = members[1].getCostPerHour();
             EV = (estimated / worked) * workers * salary;
         } else {
-            System.out.println("The weeks you entered does not exist ");
+            System.out.println("The week you entered is invalid ");
         }
         return EV;
     }
@@ -29,7 +29,7 @@ public class RamziTes {
         Workpackage[] workPackages = convertJsonToObject("C:\\Users\\ramzi\\Documents\\Minigit\\project-group-1\\src\\JsonFiles\\WorkPackages.json", Workpackage[].class);
         Member[] members = convertJsonToObject("C:\\Users\\ramzi\\Documents\\Minigit\\project-group-1\\src\\JsonFiles\\Members.json", Member[].class);
         System.out.println("------------------------------");
-        System.out.println(sprintEV(members, workPackages,1));
+        System.out.println(EV(members, workPackages,2));
 
     }
 }
