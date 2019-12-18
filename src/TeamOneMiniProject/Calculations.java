8585968eccf5dd45ac7d6321e278f1e0647caf3f
Johann <gustammjo@student.gu.se>
2019-12-18 09:55:40 +0100

    Put EVERY calculatioin in calculus class

--- src/TeamOneMiniProject/Calculations.java
+++ src/TeamOneMiniProject/Calculations.java
@@ -1,12 +1,27 @@
 package TeamOneMiniProject;
 
+import java.util.HashMap;
+
+
 public class Calculations {
 
-    public static Double calculateSumHours (Member[] members){
+    public static Double getHoursOnProject (Member[] members){
         Double  sum = 0.0;
         for (int i = 0; i < members.length; i++){
             sum += members[i].getHoursWorked();
         }
         return sum;
+    }
+
+    public static HashMap<String, Double> getHoursPerMember (Member [] members){
+        HashMap<String, Double> hoursPerMember = new HashMap<>();
+
+        for (int i = 0; i < members.length; i++){
+            String name = members[i].getFullName();
+            Double hours = members[i].getHoursWorked();
+            hoursPerMember.put(name, hours);
+        }
+        return hoursPerMember;
+
     }
 }
--- src/test/TestJohann.java
+++ src/test/TestJohann.java
@@ -2,17 +2,28 @@ package test;
 import TeamOneMiniProject.Member;
 import TeamOneMiniProject.Calculations;
 
+import java.util.HashMap;
+
 import static TeamOneMiniProject.JsonFileHandling.convertJsonToObject;
 
 public class TestJohann {
 
     public static void main (String [] args) {
+
         Member[] members = convertJsonToObject("/Users/Johann/IdeaProjects/project-group-1/src/JsonFiles/Members.json", Member[].class);
-        for(int i = 0; i < members.length; i ++){
-            System.out.println(members[i].getFullName() + ": " + (members[i]).getHoursWorked());
-        }
+
+        System.out.println(Calculations.getHoursPerMember(members));
         System.out.println();
-        System.out.println("Total: " + Calculations.calculateSumHours(members));
+
+        HashMap<String,Double> hoursPerMember = Calculations.getHoursPerMember(members);
+        for (String key : hoursPerMember.keySet()){
+            System.out.println(key + ": " + hoursPerMember.get(key));
+        }
+
+
+
+        System.out.println();
+        System.out.println("Total: " + Calculations.getHoursOnProject(members));
 
     }
 
