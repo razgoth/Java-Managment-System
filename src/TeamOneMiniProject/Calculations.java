ef1152b6d830dd70dc43a67065163e4351e4b99b
sramzi <sramzi@student.chalmers.se>
2019-12-19 06:04:02 +0100

    created a method for project schedule and some comments for RamziTes

--- src/TeamOneMiniProject/Calculations.java
+++ src/TeamOneMiniProject/Calculations.java
@@ -5,7 +5,7 @@ public class Calculations {
     public static double EV(Member[] members, Workpackage[] workpackages, int weeks) {
         double EV = 0;
         int week = weeks - 1;
-        if (weeks > 0 && weeks < workpackages.length&& (weeks%2==0)) {
+        if (weeks > 0 && weeks < workpackages.length && (weeks % 2 == 0)) {
             double estimated = workpackages[week].getEstimatedHours() + workpackages[week + 1].getEstimatedHours();
             double worked = workpackages[week].getWorkedHours() + workpackages[week + 1].getWorkedHours();
             int workers = workpackages[week].getId().length + workpackages[week + 1].getId().length;
@@ -16,5 +16,16 @@ public class Calculations {
         }
         return EV;
     }
-    
+
+    public static void projectSunshine(Project[] project) {
+
+        double startWeek = project[0].getStartWeek();
+        double endWeek = project[0].getEndWeek();
+        String activity = project[0].getProjectName();
+
+        System.out.println(startWeek);
+        System.out.println(endWeek);
+        System.out.println(activity);
+
+    }
 }
