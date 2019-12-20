47021df51c8bc53805422c6213b8c04e087fd0f2
frejat <frejat@student.chalmers.se>
2019-12-20 13:16:57 +0000

    correcting

--- src/TeamOneMiniProject/Calculations.java
+++ src/TeamOneMiniProject/Calculations.java
@@ -7,26 +7,6 @@ import java.util.Scanner;
 
 public class Calculations {
 	
-	
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	public static Member retrieveMember(Member[] members, String id) {
 		for(int i = 0; i < members.length; i++) {
 			if(members[i].getId().equals(id)) {
@@ -35,7 +15,7 @@ public class Calculations {
 		}
 		return null;
 	}
-	
+
 	public static void printHoursHash(Member foundMember){
 		double totHours = 0;
    		if(foundMember != null) {
@@ -49,24 +29,24 @@ public class Calculations {
    		} else {
    			System.out.println("A member with input ID was not found");
    		}
-		
+
 	}
-	
+
 	public static void printPackagesWorked(Workpackage[] workpackages, Member foundMember3, String id) {
-		
+
 		String output = "";
 		String wp = "";
 		String name = "";
-		
+
 		// Checks if id exists in members and catches name.
 		if (foundMember3 != null) {
 			name = foundMember3.getFullName();
 		}
-		
-		
+
+
 		//Checks if id exists in workpackages and catches workpackages participated in
 		for(int i = 0; i < workpackages.length; i++) {
-			
+
 			// Iterates Id-array within workpackage-array
 			for(int j = 0; j < workpackages[i].getId().length; j++) {
 				if(workpackages[i].getIdValue(j).equals(id)) {
@@ -74,7 +54,7 @@ public class Calculations {
 				}
 			}
 		}
-		
+
 		// Checks if data was found and output found data.
 		if(name.equals("") && wp.isEmpty()) {
 			output = "ID "+id+" does not exist in the system.\n";
@@ -87,24 +67,24 @@ public class Calculations {
 			output = "Workpackages "+name+" participated in: \n";
 			output += wp;
 		}
-		
-		
+
+
 		System.out.println(output);
 	}
-	
+
 	public static ArrayList<String> packagesWorkedGold(Workpackage[] workpackages, Member foundMember3, String id) {
 		ArrayList<String> wp = new ArrayList<String>();
 		String name = "";
-		
+
 		// Checks if id exists in members and catches name.
 		if (foundMember3 != null) {
 			name = foundMember3.getFullName();
 		}
-		
-		
+
+
 		//Checks if id exists in workpackages and catches workpackages participated in
 		for(int i = 0; i < workpackages.length; i++) {
-					
+
 			// Iterates Id-array within workpackage-array
 			for(int j = 0; j < workpackages[i].getId().length; j++) {
 				if(workpackages[i].getIdValue(j).equals(id)) {
@@ -112,8 +92,8 @@ public class Calculations {
 				}
 			}
 		}
-		
-		
+
+
 		// Checks if data was found and output found data.
 		if(name.equals("") && wp.isEmpty()) {
 			System.out.println("ID "+id+" does not exist in the system");
@@ -122,8 +102,8 @@ public class Calculations {
 		} else if(!(wp.isEmpty()) && name.equals("")){
 			System.out.println("No member with ID "+id+" could be found.The ID was found in workpackages:");
 		}
-				
+
 		return wp;
 	}
-	
+
 }
